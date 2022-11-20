5294f29423adc5e4c4b5a0ce97d970e35feb0fe9
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-20 19:09:59 +0530

    checking for leap year

--- index.js
+++ index.js
@@ -102,18 +102,18 @@ function checkPalindromeForAllDateFormats(date){
     return flag;
   }
 
-// function isLeapYear(year){
-//     if(year % 400 ===0){
-//         return true;
-//     }
-//     if(year % 100 === 0){
-//         return false;
-//     }
-//     if(year % 4 === 0){
-//         return true;
-//     }
-//     return false;
-// }
+function isLeapYear(year){
+    if(year % 400 ===0){
+        return true;
+    }
+    if(year % 100 === 0){
+        return false;
+    }
+    if(year % 4 === 0){
+        return true;
+    }
+    return false;
+}
 
 // function getNextDate(date){
 //     var day = date.day +1;  //increasing the day
