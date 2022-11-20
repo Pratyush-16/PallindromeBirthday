fa7cecb28c431d16f253cdab6fd64c40d0e63412
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-20 19:08:12 +0530

    getting all date formats and converting into string

--- index.js
+++ index.js
@@ -72,18 +72,18 @@ function convertDateToString(date){
 //console.log(convertDateToString(date));
 
 
-// function getAllDateFormats(date) {
-//     var dateStr = convertDateToString(date);
+function getAllDateFormats(date) {
+    var dateStr = convertDateToString(date);
 
-//     var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year
-//     var mmddyyyy = dateStr.month + dateStr.day + dateStr.year
-//     var yyyymmdd = dateStr.year + dateStr.month + dateStr.day
-//     var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2)
-//     var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2)
-//     var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2)
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2)
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day
 
-//     return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
-// }
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
+}
 
 //console.log(getAllDateFormats(date));
 
