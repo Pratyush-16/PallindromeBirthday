85cdb2b1aeacab7f8a84438e3598f04eb930733b
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-20 19:07:09 +0530

    conversion of date to string function

--- index.js
+++ index.js
@@ -49,25 +49,25 @@ function isPalindrome(str){
 //     year:2020
 // }
 
-// function convertDateToString(date){ 
-//     var dateStr = {day:'', month:'', year:''};
+function convertDateToString(date){ 
+    var dateStr = {day:'', month:'', year:''};
 
-//     if(date.day < 10) {
-//         dateStr.day ='0'+ date.day;
-//     }else{
-//         dateStr.day = date.day.toString();
-//     }
+    if(date.day < 10) {
+        dateStr.day ='0'+ date.day;
+    }else{
+        dateStr.day = date.day.toString();
+    }
 
-//     if(date.month < 10){
-//         dateStr.month = '0'+ date.month;
-//     }else{
-//         dateStr.month = date.month.toString();
-//     }
+    if(date.month < 10){
+        dateStr.month = '0'+ date.month;
+    }else{
+        dateStr.month = date.month.toString();
+    }
 
-//     dateStr.year = date.year.toString();
+    dateStr.year = date.year.toString();
 
-//     return dateStr;
-// }
+    return dateStr;
+}
 
 //console.log(convertDateToString(date));
 
